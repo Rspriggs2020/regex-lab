df77a923a4b6b80ece6215812ec600148eab556b
ryliespriggs <rspriggs812@gmail.com>
2021-03-08 12:32:46 -0800

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    !!word.capitalize.match(/\A+[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
+   words = text.split(" ")
+   words.grep(/\b\w{5}\b/)
+
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z].*[\.,;:]$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
 end
